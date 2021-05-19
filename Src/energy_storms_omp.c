61ac24699d799d65d783068bef6d1f1c6d35d8d2
rubenandrebarreiro <ruben.andre.letra.barreiro@gmail.com>
2021-05-19 15:59:07 +0100

    Some minor changes and Parallelisation of the Initialisation Loop

--- Src/energy_storms_omp.c
+++ Src/energy_storms_omp.c
@@ -20,29 +20,34 @@
 #include<sys/time.h>
 #include "omp.h"
 
-/* Function to get wall time */
-double cp_Wtime(){
+/* Function to compute the wall time */
+double compute_wall_time(){
+
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return tv.tv_sec + 1.0e-6 * tv.tv_usec;
-}
 
+    return (double) tv.tv_sec + (1.0e-6 * (double) tv.tv_usec);
+}
 
 #define THRESHOLD    0.001f
 
 /* Structure used to store data for one storm of particles */
 typedef struct {
     int size;    // Number of particles
-    int *posval; // Positions and values
+    int *position_values; // Positions and values
 } Storm;
 
 /* THIS FUNCTION CAN BE MODIFIED */
 /* Function to update a single position of the layer */
 void update( float *layer, int layer_size, int k, int pos, float energy ) {
+
     /* 1. Compute the absolute value of the distance between the
         impact position and the k-th position of the layer */
     int distance = pos - k;
-    if ( distance < 0 ) distance = - distance;
+
+    if(distance < 0) {
+        distance = - distance;
+    }
 
     /* 2. Impact cell has a distance value of 1 */
     distance = distance + 1;
@@ -53,22 +58,29 @@ void update( float *layer, int layer_size, int k, int pos, float energy ) {
     float attenuation = sqrtf( (float)distance );
 
     /* 4. Compute attenuated energy */
-    float energy_k = energy / layer_size / attenuation;
+    float energy_k = (energy / layer_size / attenuation); // NOLINT(cppcoreguidelines-narrowing-conversions)
 
     /* 5. Do not add if its absolute value is lower than the threshold */
-    if ( energy_k >= THRESHOLD / layer_size || energy_k <= -THRESHOLD / layer_size )
-        layer[k] = layer[k] + energy_k;
+    if ( ( energy_k >= (THRESHOLD / layer_size) ) || ( energy_k <= (-THRESHOLD / layer_size) ) ) { // NOLINT(cppcoreguidelines-narrowing-conversions)
+        layer[k] = (layer[k] + energy_k);
+    }
+
 }
 
 
 /* ANCILLARY FUNCTIONS: These are not called from the code section which is measured, leave untouched */
 /* DEBUG function: Prints the layer status */
-void debug_print(int layer_size, float *layer, int *positions, float *maximum, int num_storms ) {
-    int i,k;
+void debug_print(int layer_size, float *layer, const int *positions, const float *maximum, int num_storms ) {
+
+    int i, k;
+
     /* Only print for array size up to 35 (change it for bigger sizes if needed) */
     if ( layer_size <= 35 ) {
+
         /* Traverse layer */
+
         for( k=0; k<layer_size; k++ ) {
+
             /* Print the energy value of the current cell */
             printf("%10.4f |", layer[k] );
 
@@ -98,37 +110,40 @@ void debug_print(int layer_size, float *layer, int *positions, float *maximum, i
 /*
  * Function: Read data of particle storms from a file
  */
-Storm read_storm_file( char *fname ) {
-    FILE *fstorm = fopen( fname, "r" );
-    if ( fstorm == NULL ) {
-        fprintf(stderr,"Error: Opening storm file %s\n", fname );
+Storm read_storm_file( char *filename ) {
+
+    FILE *file_storm = fopen(filename, "r" );
+
+    if(file_storm == NULL ) {
+        fprintf(stderr, "Error: Opening storm file %s\n", filename);
         exit( EXIT_FAILURE );
     }
 
     Storm storm;    
-    int ok = fscanf(fstorm, "%d", &(storm.size) );
+    int ok = fscanf(file_storm, "%d", &(storm.size) ); // NOLINT(cert-err34-c)
+
     if ( ok != 1 ) {
-        fprintf(stderr,"Error: Reading size of storm file %s\n", fname );
+        fprintf(stderr, "Error: Reading size of storm file %s\n", filename);
         exit( EXIT_FAILURE );
     }
 
-    storm.posval = (int *)malloc( sizeof(int) * storm.size * 2 );
-    if ( storm.posval == NULL ) {
-        fprintf(stderr,"Error: Allocating memory for storm file %s, with size %d\n", fname, storm.size );
+    storm.position_values = (int *)malloc(sizeof(int) * storm.size * 2 );
+    if (storm.position_values == NULL ) {
+        fprintf(stderr, "Error: Allocating memory for storm file %s, with size %d\n", filename, storm.size );
         exit( EXIT_FAILURE );
     }
     
     int elem;
     for ( elem=0; elem<storm.size; elem++ ) {
-        ok = fscanf(fstorm, "%d %d\n",
-                    &(storm.posval[elem*2]),
-                    &(storm.posval[elem*2+1]) );
+        ok = fscanf(file_storm, "%d %d\n", // NOLINT(cert-err34-c)
+                    &(storm.position_values[elem * 2]),
+                    &(storm.position_values[elem * 2 + 1]) );
         if ( ok != 2 ) {
-            fprintf(stderr,"Error: Reading element %d in storm file %s\n", elem, fname );
+            fprintf(stderr, "Error: Reading element %d in storm file %s\n", elem, filename );
             exit( EXIT_FAILURE );
         }
     }
-    fclose( fstorm );
+    fclose(file_storm );
 
     return storm;
 }
@@ -137,55 +152,145 @@ Storm read_storm_file( char *fname ) {
  * MAIN PROGRAM
  */
 int main(int argc, char *argv[]) {
-    int i,j,k;
+
+    int i, j, k;
 
     /* 1.1. Read arguments */
-    if (argc<3) {
-        fprintf(stderr,"Usage: %s <size> <storm_1_file> [ <storm_i_file> ] ... \n", argv[0] );
+    if (argc < 4) {
+        fprintf(stderr,"Usage: %s <num_threads> <size> <storm_1_file> [ <storm_i_file> ] ... \n", argv[0] );
         exit( EXIT_FAILURE );
     }
 
-    int layer_size = atoi( argv[1] );
-    int num_storms = argc-2;
-    Storm storms[ num_storms ];
+    /* Retrieve the Number of Threads to use in the simulation */
+    int num_threads = atoi(argv[1]); // NOLINT(cert-err34-c)
+
+    /* Retrieve the size of the Layer */
+    int layer_size = atoi(argv[2] ); // NOLINT(cert-err34-c)
+
+    /* Retrieve the number of Storms */
+    int num_storms = (argc - 3);
+
+    /* Create the array of Storms */
+    Storm storms[num_storms];
 
     /* 1.2. Read storms information */
-    for( i=2; i<argc; i++ ) 
-        storms[i-2] = read_storm_file( argv[i] );
+    for(i=3; i<argc; i++) {
+        storms[(i - 3)] = read_storm_file(argv[i]);
+    }
 
-    /* 1.3. Intialize maximum levels to zero */
-    float maximum[ num_storms ];
-    int positions[ num_storms ];
+    /* 1.3. Initialise maximum levels to zero */
+    float maximum[num_storms];
+    int positions[num_storms];
+
     for (i=0; i<num_storms; i++) {
         maximum[i] = 0.0f;
         positions[i] = 0;
     }
 
+    /* Initialise the private variables for the Threads' Identification */
+
+    // The ID of the Thread (will be set as a Private Variable)
+    int thread_id;
+
+    // The current number of Thread (will be set as a Private Variable)
+    int current_thread;
+
     /* 2. Begin time measurement */
-    double ttotal = cp_Wtime();
+    double total_time = compute_wall_time();
 
     /* START: Do NOT optimize/parallelize the code of the main program above this point */
 
-    /* 3. Allocate memory for the layer and initialize to zero */
+    /* 3. Allocate memory for the layer and initialise to zero */
     float *layer = (float *)malloc( sizeof(float) * layer_size );
     float *layer_copy = (float *)malloc( sizeof(float) * layer_size );
-    if ( layer == NULL || layer_copy == NULL ) {
+
+    if (layer == NULL || layer_copy == NULL) {
         fprintf(stderr,"Error: Allocating the layer memory\n");
         exit( EXIT_FAILURE );
     }
-    for( k=0; k<layer_size; k++ ) layer[k] = 0.0f;
-    for( k=0; k<layer_size; k++ ) layer_copy[k] = 0.0f;
-    
+
+    // The number of Positions in the Layer, to be initialised, individually, by each Thread
+    int num_positions_in_layer_per_thread;
+    num_positions_in_layer_per_thread = (layer_size / num_threads);
+
+    // The number of remaining Positions in the Layer, to be initialised, by the last Thread
+    int num_remaining_positions_in_layer_for_last_thread;
+    num_remaining_positions_in_layer_for_last_thread = (layer_size % num_threads);
+
+    // The number of Positions in the Layer, to be initialised, by the last Thread
+    // for the case of the total number of Positions in the Layer, to be initialised,
+    // are not divisible for the number of Threads
+    int num_positions_in_layer_for_last_thread;
+    num_positions_in_layer_for_last_thread = (num_positions_in_layer_per_thread + num_remaining_positions_in_layer_for_last_thread);
+
+    // Sequential Code for the initialisation of the Positions, in the Layer
+    /*
+    for(k=0; k < layer_size; k++) {
+        layer[k] = 0.0f;
+    }
+
+    for(k=0; k < layer_size; k++) {
+        layer_copy[k] = 0.0f;
+    }
+    */
+
+    // Set the number of Threads given, as argument,
+    // as the flag of Number of Threads to be used by the OpenMP
+    omp_set_num_threads(num_threads);
+
+    // Parallel Loop, in OpenMP, for each Thread, individually:
+    // - Private Variables for each Thread: thread_id, current_thread
+    // - Number of Threads to be launched, in the Parallel Loop: num_threads
+    // - Shared Variables by all the Threads: num_threads, num_positions_in_layer_per_thread
+    // - NOTE: This loop is Embarrassingly Parallel and does not have any Loop-Carried Dependencies;
+    #pragma omp parallel for private(thread_id, current_thread) num_threads(num_threads) shared(num_threads, num_positions_in_layer_per_thread, layer, layer_copy)
+    for(current_thread = 0; current_thread < num_threads; current_thread++) {
+
+        // Set the Private Variable for the ID of the current Thread
+        thread_id = omp_get_thread_num();
+
+        int layer_position_offset = (current_thread * num_positions_in_layer_per_thread);
+
+        // If the current Thread's ID does not belong to the last Thread
+        if(thread_id < (num_threads - 1)) {
+
+            // For each Position in Layer to be initialised, individually, in Parallel, by each Thread
+            for(int current_position_in_layer_thread = 0; current_position_in_layer_thread < num_positions_in_layer_per_thread; current_position_in_layer_thread++) {
+
+                // Initialisations of the Layer and Layer's Copy made sequentially, in Code Fusion
+                layer[(layer_position_offset + current_position_in_layer_thread)] = 0.0f;
+                layer_copy[(layer_position_offset + current_position_in_layer_thread)] = 0.0f;
+
+            }
+
+        }
+        // If the current Thread's ID belongs to the last Thread
+        else {
+
+            // For each Position in Layer to be initialised, individually, in Parallel, by the last Thread
+            for(int current_position_in_layer_thread = 0; current_position_in_layer_thread < num_positions_in_layer_for_last_thread; current_position_in_layer_thread++) {
+
+                // Initialisations of the Layer and Layer's Copy made sequentially, in Code Fusion
+                layer[(layer_position_offset + current_position_in_layer_thread)] = 0.0f;
+                layer_copy[(layer_position_offset + current_position_in_layer_thread)] = 0.0f;
+
+            }
+
+        }
+
+    }
+
+
     /* 4. Storms simulation */
-    for( i=0; i<num_storms; i++) {
+    for(i=0; i<num_storms; i++) {
 
         /* 4.1. Add impacts energies to layer cells */
         /* For each particle */
         for( j=0; j<storms[i].size; j++ ) {
             /* Get impact energy (expressed in thousandths) */
-            float energy = (float)storms[i].posval[j*2+1] * 1000;
+            float energy = (float)storms[i].position_values[j * 2 + 1] * 1000;
             /* Get impact position */
-            int position = storms[i].posval[j*2];
+            int position = storms[i].position_values[j * 2];
 
             /* For each cell in the layer */
             for( k=0; k<layer_size; k++ ) {
@@ -219,7 +324,7 @@ int main(int argc, char *argv[]) {
     /* END: Do NOT optimize/parallelize the code below this point */
 
     /* 5. End time measurement */
-    ttotal = cp_Wtime() - ttotal;
+    total_time = (compute_wall_time() - total_time);
 
     /* 6. DEBUG: Plot the result (only for layers up to 35 points) */
     #ifdef DEBUG
@@ -228,19 +333,26 @@ int main(int argc, char *argv[]) {
 
     /* 7. Results output, used by the Tablon online judge software */
     printf("\n");
+
     /* 7.1. Total computation time */
-    printf("Time: %lf\n", ttotal );
+    printf("Time: %lf\n", total_time );
+
     /* 7.2. Print the maximum levels */
     printf("Result:");
-    for (i=0; i<num_storms; i++)
-        printf(" %d %f", positions[i], maximum[i] );
+
+    for (i=0; i<num_storms; i++) {
+        printf(" %d %f", positions[i], maximum[i]);
+    }
+
     printf("\n");
 
     /* 8. Free resources */    
-    for( i=0; i<argc-2; i++ )
-        free( storms[i].posval );
+    for(i=0; i < (argc - 3); i++) {
+        free(storms[i].position_values);
+    }
 
     /* 9. Program ended successfully */
     return 0;
+
 }
 
